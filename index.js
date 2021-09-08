788c9416cf92a559109b5421f9a38f7c33a539ec
omar-faruk02 <tuhin.omar99@gmail.com>
2021-09-08 23:50:20 +0600

    m

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(cors())
 app.use(express.json())
 
 
-
+console.log(process.env.DB_NAME)
 
 
 // const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/NewPortal?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const NewsCollection = client.db("NewPortal").collection("news");
