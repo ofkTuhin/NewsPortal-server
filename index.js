2faeaaa62f55509a38c762b380e9bf673dc2a8d3
omar-faruk02 <tuhin.omar99@gmail.com>
2021-09-09 01:32:16 +0600

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/NewPortal?retryWrites=true&w=majority`;
 
 const port=3000
 
